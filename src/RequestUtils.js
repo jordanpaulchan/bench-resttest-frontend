2b1c13b44aa17568b554f43cea8cc09a4c867a68
jordanpaulchan <jordanpaulchan@gmail.com>
2018-04-09 20:51:25 -0700

    added comments for clarity

--- src/RequestUtils.js
+++ src/RequestUtils.js
@@ -13,13 +13,19 @@ function parseResponse(response, url, fetchPages) {
 
     let { transactions } = response;
     const { page, totalCount } = response;
+
+    // if fetching more pages and the number of transactions is less than the
+    // total transactions fetch transactions on next pages
     if (fetchPages && transactions.length < totalCount) {
+      // determine number of pages remaining based on number of transactions
+      // received on first page
       const numPages = Math.floor(totalCount / transactions.length) + 1;
       const promises = [];
       for (let i = page + 1; i <= numPages; i++) {
         promises.push(fetchTransactions(url, i, false));
       }
 
+      // resolve all promises for remaining pages and concatenate the result
       Promise.all(promises)
         .then(pageTransactions => {
           success(transactions.concat(_.flatten(pageTransactions)));
@@ -48,6 +54,7 @@ export function fetchTransactions(url, page = 1, fetchPages = true) {
 }
 
 function requestPromise(url) {
+  // make request to API and return a promise with the response
   return new Promise((success, failure) => {
     axios
       .get(url)
